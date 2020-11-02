c25d42c174761eb45d022f465b82ea2e2feedfca
Buccioz <63121659+Buccioz@users.noreply.github.com>
2020-11-02 20:41:16 +0100

    Update YTDownloader.py

--- YTDownloader.py
+++ YTDownloader.py
@@ -1,13 +1,26 @@
 import pytube
 import os
+import pyfiglet
+import termcolor
 
+from termcolor import colored
 from pytube import YouTube
 from pytube.cli import on_progress
 
-url = input('Insert video URL: ')
+banner = pyfiglet.figlet_format("YouTube Downloader", font="Slant")
+print(colored(banner, 'magenta'))
+print(colored(':::::::::::::::Developed By Buccioz:::::::::::::::', 'magenta'))
+
+url = input('\n\nInsert video URL: ')  # Input URL
+
+print(colored('\n720p IS THE ONLY RESOLUTION SUPPORTED', 'red'))
+print("\n")
+
+
 yt = pytube.YouTube(url, on_progress_callback=on_progress)
 
 print(yt.title)
+
 try:
      video = yt.streams.get_highest_resolution()
      video.download()
@@ -16,4 +29,4 @@ except EOFError as err:
      print(err)
 
 else:
-     print("Download completed successfully")
+     print (colored('\n\nDownload completed successfully !', 'green'))
